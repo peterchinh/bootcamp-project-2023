4cd0822bc5c141881b350731b1912a873f1a0711
Peter Chinh <peterchinh1018@gmail.com>
2023-11-16 22:41:24 -0800

    feat: Milestone 3A - Replaced with blog type from blog schema

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import style from './blogPreview.module.css'
-import type { Blog } from '@/app/blogData'
-import Link from 'next/link';
+import React from "react";
+import style from "./blogPreview.module.css";
+// import type { Blog } from '@/app/blogData'
+import type { IBlog } from "@/database/blogSchema";
+import Link from "next/link";
 
-export default function BlogPreview(props: Blog) {
+export default function BlogPreview(props: IBlog) {
   return (
     // Generate HTML block from Blog prop
     <div className={style.div}>
       <h3>{props.title}</h3>
-      <p>Date: {props.date}</p>
+      <p>{props.date.toDateString()}</p>
       <p>{props.description}</p>
-      <Link href={`/blog/${props.slug}`}>See more</Link> 
-	  </div>
+      <Link href={`/blog/${props.slug}`}>See more</Link>
+    </div>
   );
 }
