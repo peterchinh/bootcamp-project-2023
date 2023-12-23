967d2e00cd722cfd2a1701c5ce19df8724955b72
Peter Chinh <peterchinh1018@gmail.com>
2023-12-22 22:14:50 -0800

    feat: Milestone 4 - Added comments to projectSchema

--- bootcamp-milestone-2/src/database/projectSchema.ts
+++ bootcamp-milestone-2/src/database/projectSchema.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import { IComment } from "./blogSchema"; // Share IComment from blogSchema
 
 // typescript type (can also be an interface)
 export type IProject = {
@@ -6,19 +7,30 @@ export type IProject = {
   description: string;
   image: string; //for project image
   link: string; //link to project
-  slug: string
+  slug: string;
+  comments: IComment[]; // array for comments
 };
+
+
+// commentSchema for projectSchema
+const commentSchema = new Schema<IComment>({
+  user: { type: String, required: true },
+  comment: { type: String, required: true },
+  time: { type: Date, required: true },
+});
 
 // mongoose schema
 const projectSchema = new Schema<IProject>({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  image: {type: String, required: true},
-  link: {type: String, required: true},
-  slug: {type: String, required: true},
+  image: { type: String, required: true },
+  link: { type: String, required: true },
+  slug: { type: String, required: true },
+  comments: [{ type: commentSchema, required: true }],
 });
 
 // defining the collection and model
-const Project = mongoose.models["projects"] || mongoose.model("projects", projectSchema);
+const Project =
+  mongoose.models["projects"] || mongoose.model("projects", projectSchema);
 
 export default Project;
